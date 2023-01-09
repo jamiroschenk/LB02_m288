abcc02faff5c76b5f1a9ffd84842b088856b0d4c
jamiro.schenk <schenkj@bzz.ch>
2023-01-09 21:31:41 +0100

    akutalisiert

---
+++ JavaScript_LB02.js
@@ -0,0 +1,5 @@
+const response = fetch('https://api.exchangeratesapi.io/latest?base=CHF&access_key=gU8bH9lVm1Pr3VdsTV1zAS9D0M5GT3EE');
+const data = response.json();
+console.log(data)
+// gU8bH9lVm1Pr3VdsTV1zAS9D0M5GT3EE
+
