d55190fe399f7b772342a4175a5baf79f4f25a9a
jamiro.schenk <schenkj@bzz.ch>
2023-01-10 21:48:11 +0100

    akutalisiert

--- JavaScript_LB02.js
+++ JavaScript_LB02.js
@@ -1,5 +1,17 @@
-const response = fetch('https://api.exchangeratesapi.io/latest?base=CHF&access_key=gU8bH9lVm1Pr3VdsTV1zAS9D0M5GT3EE');
-const data = response.json();
-console.log(data)
-// gU8bH9lVm1Pr3VdsTV1zAS9D0M5GT3EE
+// HTML input Felder
+startbetrag = document.querySelector("#startbetrag");
+endbetrag = document.querySelector("#endbetrag");
+währung1 = document.querySelector("#währung1");
+währung2 = document.querySelector("#währung2");
 
+// API Link
+api_link = 'https://v6.exchangerate-api.com/v6/3e1614e0b75007cb331cfbfa/pair/'
+
+// events, bei denen api abgefragt wird
+startbetrag.addEventListener('change', go);
+währung1.addEventListener('change', go);
+währung2.addEventListener('change', go);
+
+function go(schwarzes_Loch) {
+    console.log(schwarzes_Loch.target.value);
+}
