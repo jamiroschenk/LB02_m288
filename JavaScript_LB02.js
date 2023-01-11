6b179474c8e6b27ff6567e180c7bddcfba193625
jamiro.schenk <schenkj@bzz.ch>
2023-01-11 20:06:22 +0100

    akutalisiert

--- JavaScript_LB02.js
+++ JavaScript_LB02.js
@@ -7,11 +7,22 @@ währung2 = document.querySelector("#währung2");
 // API Link
 api_link = 'https://v6.exchangerate-api.com/v6/3e1614e0b75007cb331cfbfa/pair/'
 
-// events, bei denen api abgefragt wird
+// Events, bei denen api abgefragt wird
 startbetrag.addEventListener('change', go);
 währung1.addEventListener('change', go);
 währung2.addEventListener('change', go);
 
-function go(schwarzes_Loch) {
-    console.log(schwarzes_Loch.target.value);
+function go() {
+    if (währung1.value != "Währung" && währung2.value != "Währung" && startbetrag.value != "") {
+        fetch(api_link + währung1.value + "/" + währung2.value)
+            .then((request) => request.json())
+            .then((get_exchangerate) => get_exchangerate.conversion_rate)
+            .then((claculate_result))
+    } 
+}
+
+function claculate_result(exchangerate) {
+    endbetrag.value = (() => {
+        return exchangerate * startbetrag.value
+    })()
 }
